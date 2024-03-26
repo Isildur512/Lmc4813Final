456a7fd7c5c2f971fbba44a13cb03325af3a4066
Isildur512 <78391349+Isildur512@users.noreply.github.com>
2024-03-26 13:14:41 -0400

    Added "Hello World" file

---
+++ ScreenTest/ScreenTest.ino
@@ -0,0 +1,11 @@
+#include "Nokia_5110.h"
+Nokia_5110 lcd = Nokia_5110(1, 2, 10, 13, 14);
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lcd.println("Hello World!");
+}
