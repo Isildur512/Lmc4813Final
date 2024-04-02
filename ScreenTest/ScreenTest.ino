a79dee8257d36c3558aaf15f6a72da74e08414f3
Isildur512 <78391349+Isildur512@users.noreply.github.com>
2024-04-02 13:26:23 -0400

    Added state machine and dictionary support
    You will need to download the dictionary library. This can be done within the Arduino ide

--- ScreenTest/ScreenTest.ino
+++ ScreenTest/ScreenTest.ino
@@ -1,11 +1,28 @@
 #include "Nokia_5110.h"
+#include "Dictionary.h"
 Nokia_5110 lcd = Nokia_5110(1, 2, 10, 13, 14);
+
+enum {TUTORIAL, HUNTING, POLLINATING} state = TUTORIAL;
+Dictionary flowers = new Dictionary();
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  flowers("1", "Azalea");
 }
 
 void loop() {
+  delay(1000);
   // put your main code here, to run repeatedly:
-  lcd.println("Hello World!");
+  if (state == TUTORIAL) {
+    Serial.println("Hello World!");
+  } else if (state == HUNTING) {
+    Serial.println("It's arduinoing time");
+  }
+  delay(500);
+  state = HUNTING;
+}
+
+void tutorial() {
+  
 }
