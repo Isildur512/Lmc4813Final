fde41300f89bc0e9c543896a4ab7adf13283dd6d
Isildur512 <78391349+Isildur512@users.noreply.github.com>
2024-04-02 13:39:39 -0400

    Made a simple slide system

--- ScreenTest/ScreenTest.ino
+++ ScreenTest/ScreenTest.ino
@@ -4,6 +4,7 @@ Nokia_5110 lcd = Nokia_5110(1, 2, 10, 13, 14);
 
 enum {TUTORIAL, HUNTING, POLLINATING} state = TUTORIAL;
 Dictionary flowers = new Dictionary();
+int curSlide = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,14 +16,32 @@ void loop() {
   delay(1000);
   // put your main code here, to run repeatedly:
   if (state == TUTORIAL) {
-    Serial.println("Hello World!");
+    tutorial();
   } else if (state == HUNTING) {
-    Serial.println("It's arduinoing time");
+    hunting();
+  } else {
+    pollinating();
   }
   delay(500);
   state = HUNTING;
 }
 
 void tutorial() {
-  
+  String tutorialText[] = {
+    "Hello! Welcome to Bees, Knees, and RFIDs: A Pollination Simulation! /n /n Press the button to go to the next slide.",
+    "You will be directed to four different flower beds around campus, on a mission to carry the bee to each. /n /n Press the button to go to the next slide.",
+    "When you reach a flower bed, press the bee's face directly into the special flower, and wait for the pollination process to finish! /n /n Press the button to go to the next slide.",
+    "From there, you'll be promtped to go to the next flower bed! /n /n Press the button to go to the next slide."
+    "Would you like to see this again? /n Yes /n No",
+    "Alright, good luck!"
+  };
+  Serial.println(tutorialText[0]);
+}
+
+void hunting() {
+
+}
+
+void pollinating() {
+
 }
